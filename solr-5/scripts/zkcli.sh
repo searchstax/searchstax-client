831ad240240f638caf8f175f7a843428ca74427e
Sameer Maggon <sameer@sameermaggon.com>
2016-09-01 08:12:13 -0700

    Increased buffer size

--- solr-5/scripts/zkcli.sh
+++ solr-5/scripts/zkcli.sh
@@ -9,5 +9,5 @@ JVM="java"
 
 sdir="`dirname \"$0\"`"
 
-PATH=$JAVA_HOME/bin:$PATH $JVM -Dlog4j.configuration=file:$sdir/log4j.properties -classpath "$sdir/../lib/*:$sdir/../lib/ext/*" org.apache.solr.cloud.ZkCLI ${1+"$@"}
+PATH=$JAVA_HOME/bin:$PATH $JVM -Djute.maxbuffer=10485761 -Dlog4j.configuration=file:$sdir/log4j.properties -classpath "$sdir/../lib/*:$sdir/../lib/ext/*" org.apache.solr.cloud.ZkCLI ${1+"$@"}
 
